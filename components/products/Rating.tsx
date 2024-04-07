b905d27e3aa2f56aa05d84ea891dacad1caee592
abhishekrj02 <22bec006@nith.ac.in>
2024-04-08 00:28:20 +0530

    final

--- components/products/Rating.tsx
+++ components/products/Rating.tsx
@@ -1,7 +1,7 @@
 export const Rating = ({ caption }: { caption: string }) => {
   return (
     <div className="flex gap-2 ">
-      {caption && <span className="text-[12px]">{caption}</span>}
+      {/* {caption && <span className="text-[12px]">{caption}</span>} */}
     </div>
   );
 };
